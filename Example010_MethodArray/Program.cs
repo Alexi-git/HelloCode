491a39a1d4ef0e328d9e140fdad1976623018adf
Alexi <kukushkin-94@rambler.ru>
2023-03-04 11:30:09 +0300

    Example 009 IntroArray

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,7 +1,7 @@
-﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18 };
+﻿int[] array = {1, 12, 31, 4, 59, 15, 16, 31, 17, 18 };
 
 int n = array.Length;
-int find = 4;
+int find = 59;
 
 int index = 0;
 
@@ -9,7 +9,10 @@ while (index < n)
 {
 
     if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
     //index = index + 1;
     index++;
 }
-Console
